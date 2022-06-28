1e327623632a0544e1416e835a768bf3fc563d7a
kuro0993 <kuro0993@gmail.com>
2022-06-28 13:02:06 +0900

    add simple-calendar

--- Gemfile
+++ Gemfile
@@ -72,3 +72,5 @@ group :test do
 end
 
 gem 'solargraph', group: :development
+
+gem "simple_calendar", "~> 2.4"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_16_233303) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_28_034744) do
   create_table "articles", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -27,6 +27,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_16_233303) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "events", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
