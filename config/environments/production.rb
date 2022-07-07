6b467aba9afd8e7281f8a3619ecbfe2d6db29fdf
kuro0993 <kuro0993@gmail.com>
2022-07-08 06:14:59 +0900

    fix allow host: miss commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+
   # allow hosts
-  config.hosts << ENV["ALLOW_HOST"]
+  # config.hosts << ENV["ALLOW_HOST"]
 end
