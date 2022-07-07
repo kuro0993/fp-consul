73d13fd34eb140f8d6e7fcbe0e4ce545dfac040a
kuro0993 <kuro0993@gmail.com>
2022-07-07 10:41:15 +0900

    allow host (production)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # allow requests
+  config.hosts << "fp-consul-rails-jurf47trpa-dt.a.run.app"
 end
